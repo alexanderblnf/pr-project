f42dfb5e37a773cf9e1f7891c1929ef0ac5677a7
alexanderblnf <alexandru@g2g-online.nl>
2019-01-17 12:11:34 +0100

    Added step to feature selection

--- feature_selection.m
+++ feature_selection.m
@@ -1,18 +1,18 @@
 %% Tests various feature selection techniques and chooses the best one
-function [error_curves, optimal_feats] = feature_selection(num_feats, trn, tst, classf)
+function [error_curves, optimal_feats] = feature_selection(num_feats, trn, tst, classf, step)
     % Individual selection
     [w_i, r_i] = featseli(trn, 'eucl-m', num_feats);
-    e_i = clevalf(trn * w_i, classf, [1:1:num_feats], [], 2, tst * w_i);
+    e_i = clevalf(trn * w_i, classf, [1:step:num_feats], [], 2, tst * w_i);
     e_i.names = strcat(e_i.names, '-individual');
     
     % Backward selection
     [w_b, r_b] = featselb(trn, 'eucl-m', num_feats);
-    e_b = clevalf(trn * w_b, classf, [1:1:num_feats], [], 2, tst * w_b);
+    e_b = clevalf(trn * w_b, classf, [1:step:num_feats], [], 2, tst * w_b);
     e_b.names = strcat(e_b.names, '-backward');
     
     % Forward selection
     [w_f, r_f] = featself(trn, 'eucl-m', num_feats);
-    e_f = clevalf(trn * w_f, classf, [1:1:num_feats], [], 2, tst * w_f);
+    e_f = clevalf(trn * w_f, classf, [1:step:num_feats], [], 2, tst * w_f);
     e_f.names = strcat(e_f.names, '-forward');
     
     error_curves = {e_i, e_b, e_f};
