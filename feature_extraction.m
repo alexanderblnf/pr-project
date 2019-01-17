1a7f83d749351cdeadf6df6965f4672d10c42c06
Bjonkt <frankie.gallagher@hotmail.com>
2019-01-17 22:05:40 +0100

    Update feature_extraction.m

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,9 +1,9 @@
-function [nist_feat_pca, nist_profile_pca, nist_pix_pca, nist_dis_pca] = feature_extraction(nist_feat, nist_profile, nist_pix, nist_dis,processed_data,percent);
+function [nist_feat_pca, nist_profile_pca, nist_pix_pca, nist_dis_pca] = feature_extraction(processed_data_set,percent);
     
-    nist_feat_pca = nist_feat*pca(processed_data,percent);
-    nist_profile_pca = nist_profile*pca(processed_data,percent);
-    nist_pix_pca = nist_pix*pca(processed_data,percent);
-    nist_dis_pca = nist_dis*pca(processed_data,percent);
+    nist_feat_pca = im_features(processed_dataset, processed_dataset, 'all')*pca(processed_data_set,percent);
+    nist_profile_pca = im_profile(processed_dataset, 40, 40)*pca(processed_data_set,percent);
+    nist_pix_pca = processed_dataset*pca(processed_dataset,percent);
+    nist_dis_pca = processed_dataset*proxm(processed_dataset)*pca(processed_data_set,percent);
     
 end
 
