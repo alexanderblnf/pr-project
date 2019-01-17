ac253434ca96b29592bad1c44e20f448aebdc0ad
alexanderblnf <alexandru@g2g-online.nl>
2019-01-17 20:31:32 +0100

    Finalized feature selection analysis

--- feature_selection.m
+++ feature_selection.m
@@ -1,22 +1,29 @@
 %% Tests various feature selection techniques and chooses the best one
-function [error_curves, optimal_feats] = feature_selection(num_feats, trn, tst, classf, step)
+function [error_curves, optimal_feats] = feature_selection(num_feats, trn, tst, classf, step, only_individual)
+    
     % Individual selection
     [w_i, r_i] = featseli(trn, 'eucl-m', num_feats);
-    e_i = clevalf(trn * w_i, classf, [1:step:num_feats], [], 2, tst * w_i);
+    e_i = clevalf(trn * w_i, classf, [1:step:num_feats], [], 1, tst * w_i);
     e_i.names = strcat(e_i.names, '-individual');
     
-    % Backward selection
-    [w_b, r_b] = featselb(trn, 'eucl-m', num_feats);
-    e_b = clevalf(trn * w_b, classf, [1:step:num_feats], [], 2, tst * w_b);
-    e_b.names = strcat(e_b.names, '-backward');
+    if only_individual == true
+        error_curves = {e_i};
+        feature_mapping = {r_i(:, 3)'};
+    else
+        % Backward selection
+        [w_b, r_b] = featselb(trn, 'eucl-m', num_feats);
+        e_b = clevalf(trn * w_b, classf, [1:step:num_feats], [], 1, tst * w_b);
+        e_b.names = strcat(e_b.names, '-backward');
     
-    % Forward selection
-    [w_f, r_f] = featself(trn, 'eucl-m', num_feats);
-    e_f = clevalf(trn * w_f, classf, [1:step:num_feats], [], 2, tst * w_f);
-    e_f.names = strcat(e_f.names, '-forward');
-    
-    error_curves = {e_i, e_b, e_f};
-    feature_mapping = {r_i(:, 3)', w_b{:, 1}, r_f(:, 3)'};
+        % Forward selection
+        [w_f, r_f] = featself(trn, 'eucl-m', num_feats);
+        e_f = clevalf(trn * w_f, classf, [1:step:num_feats], [], 1, tst * w_f);
+        e_f.names = strcat(e_f.names, '-forward');
+
+        error_curves = {e_i, e_b, e_f};
+        feature_mapping = {r_i(:, 3)', w_b{:, 1}, r_f(:, 3)'};
+    end
+   
     optimal_feats = get_optimal_feats(error_curves, feature_mapping);
 end
 
