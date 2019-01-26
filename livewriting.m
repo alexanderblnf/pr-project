07c0c89afc615e272fbcddb65dbe6ce99b17d1e1
Bjonkt <frankie.gallagher@hotmail.com>
2019-01-26 21:20:19 +0100

    Update
    Fixed the fat one

--- livewriting.m
+++ livewriting.m
@@ -1,36 +1,44 @@
-%% Gathering the Numbers
-I = imread('scn.png');
-gray = rgb2gray(I);         %Gray Scale
-mBW = mean(gray(:));        
-sBW = std(double(gray(:)));
-th = mBW - 1*sBW;
-BW = gray < th;             % Binary Image
-I = medfilt2(BW,[1 1]);
+%% Automatically cut numbers and dd labels
+clear all; close all; clc;
+% Load image and convert to black and white image
+I = imread('scn.png');  % Load image
+J = rgb2gray(I);        % convert to gray Scale
+m = mean(J(:));        
+s = std(double(J(:)));
+I = J < (m - 1*s);
+% Filter some noise
+I = medfilt2(I,[1 1]);
 I = imerode(I,strel('square',10));
 I = imdilate(I,strel('square',5));
-show(I)
-%%
-digits = [];
-a=prdataset([]);
-rpro = regionprops(I,'BoundingBox','Area');
+show(I) % show the scan
+% set bouding box
+nmbr=[];
+rp = regionprops(I,'BoundingBox','Area');
 j = 1;
-for i=1:size(rpro)
-    if rpro(i).Area > 1e3
-        ob = imcrop(I,(rpro(i).BoundingBox));
+test = imcrop(I,(rp(10).BoundingBox));
+imshow(test);
+% Apply bounding box
+for i=1:size(rp)
+    if rp(i).Area > 1e3
+        ob = I;
+        ob = imcrop(ob,(rp(i).BoundingBox));
+        % hacky resizing  
+        [H,W] = size(ob);
+        tmp = zeros(max(H,W),max(H,W));
+        tmp(1:H,1:W)=ob;
+        ob = tmp;
         ob = imresize(ob,[240 240]);
-        digits(:,:,j) = ob;
-        rectangle('Position',rpro(i).BoundingBox,'EdgeColor','yellow')
+        nmbr(:,:,j) = ob;
+        rectangle('Position',rp(i).BoundingBox,'EdgeColor','yellow')
         j = j+1;
     end
 end
-%%
-live_dataset = im2obj(digits);
-[o,f] = size(live_dataset);
-labels_digits = [];
-for i=1:o
-    show(live_dataset(i,:))
-    i_dgt = input('digit');
-    labels_digits = [labels_digits i_dgt];
+% Add labels to the digits
+img = im2obj(nmbr);
+lab = [];
+for i=1:size(img)
+    show(img(i,:))
+    i_nmbr = input('digit_');
+    lab = [lab i_nmbr];
 end
-
-live_dataset = setlabels(live_dataset,labels_digits');
+live_data_set = setlabels(img,lab');
